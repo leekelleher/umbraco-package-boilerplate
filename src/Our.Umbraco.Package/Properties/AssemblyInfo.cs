1a215ed206089c71310da6543feda22e514b5b0a
leekelleher <leekelleher@gmail.com>
2013-04-08 00:33:24 +0100

    Added stub csproj.
    Include AssemblyInfo and VersionInfo.

---
+++ src/Our.Umbraco.Package/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Our.Umbraco.Package")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The Umbraco Community")]
+[assembly: AssemblyProduct("Our.Umbraco.Package")]
+[assembly: AssemblyCopyright("Copyright \xa9 The Umbraco Community 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("87f49b1c-c55e-4e77-9771-0e40dd287006")]
