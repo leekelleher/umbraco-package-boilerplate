c9d114d46b5e5ad6e3f7e00f19dbd696f5e9b071
leekelleher <leekelleher@gmail.com>
2013-04-10 12:40:03 +0100

    Added batch script to fetch the MSBuild tasks from NuGet.

--- build.cmd
+++ build.cmd
@@ -1,2 +1,10 @@
+@ECHO OFF
+
+:: Fetch the MSBuild tasks (via NuGet)
+CALL .\tools\fetch-msbuild-tasks.cmd
+
+:: Build the package zip
 C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe package\package.proj
+
 :: PAUSE
+@ECHO ON
