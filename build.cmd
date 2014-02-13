2b91533f69c50b56d0f5889a09da575176b3f79f
leekelleher <leekelleher@gmail.com>
2014-02-13 23:12:56 +0000

    Removed the 'fetch-msbuild-tasks' batch file.
    As we include the MSBuild tasks with the boilerplate solution.

--- build.cmd
+++ build.cmd
@@ -1,10 +1,8 @@
-@ECHO OFF
-
-:: Fetch the MSBuild tasks (via NuGet)
-CALL .\tools\fetch-msbuild-tasks.cmd
-
 :: Build the package zip
 C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe package\package.proj
 
-:: PAUSE
-@ECHO ON
+@IF %ERRORLEVEL% NEQ 0 GOTO err
+@EXIT /B 0
+:err
+@PAUSE
+@EXIT /B 1
